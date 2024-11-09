343abfe4d63ee7ac3eebee656013240a97acb606
Diego Luis Mayolo <diego.mayolo@outlook.com>
2024-11-08 23:23:37 -0300

    fix: correct cpf validation

--- src/main/java/com/desafiovotacao/utils/validator/CpfOrCnpj.java
+++ src/main/java/com/desafiovotacao/utils/validator/CpfOrCnpj.java
@@ -1,8 +1,18 @@
 package com.desafiovotacao.utils.validator;
 
 import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = CpfOrCnpjValidator.class)
 public @interface CpfOrCnpj {
     String message() default "O número informado não é válido. Informe um CPF ou CNPJ válido, sem pontos, traços ou barras.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 }
