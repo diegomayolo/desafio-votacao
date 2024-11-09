4132cba43687c4a48206b710c4cdd0f34b6e6c8c
Diego Luis Mayolo <diego.mayolo@outlook.com>
2024-11-08 23:26:00 -0300

    refactor: renamed to SwaggerConfig

--- src/main/java/com/desafiovotacao/api/v1/config/SwaggerConfig.java
+++ src/main/java/com/desafiovotacao/api/v1/config/SwaggerConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class SpringFoxConfig {
+public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI().info(new Info().title("Desafio Votação <DB>")
